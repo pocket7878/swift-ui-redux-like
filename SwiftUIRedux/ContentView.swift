c4613a5e6e7ac488d39eeddbeabf432bc4af113c
Pocket7878 <poketo7878@gmail.com>
2019-06-04 23:39:07 +0900

    Initial Commit

---
+++ SwiftUIRedux/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUIRedux
+//
+//  Created by 十亀眞怜 on 2019/06/04.
+//  Copyright © 2019 Pocket7878. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
